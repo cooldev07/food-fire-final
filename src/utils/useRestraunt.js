b69b9034a4c4e4c94f1879ad00d4667533c950d0
cooldev07 <gogulinve@gmail.com>
2024-07-28 20:31:14 +0530

    Update useRestraunt.js

--- src/utils/useRestraunt.js
+++ src/utils/useRestraunt.js
@@ -7,9 +7,21 @@ const useRestraunt=function(){
 fetchData();
     },[])
     async function fetchData(){
-    const req=await fetch(LISTOFRESTAURANT)
-const json=await req.json();
-setResList(json)
+//     const req=await fetch(LISTOFRESTAURANT)
+// const json=await req.json();
+// setResList(json)
+        const req = fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01420&lng=76.99410&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')}`)
+  .then(response => {
+    if (response.ok) return response.json();
+    throw new Error('Network response was not ok.');
+  })
+  .then(data => {
+    const jsonObject = JSON.parse(data.contents);
+    console.log(jsonObject);
+    setResList(jsonObject)
+    // Now jsonObject is a JavaScript object
+  })
+  .catch(error => console.error('Error:', error));
 }
 return resList
 }
