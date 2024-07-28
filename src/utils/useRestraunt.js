28821666a662d1a6954d6f71ad7cb14874e071e2
cooldev07 <gogulinve@gmail.com>
2024-07-28 21:21:32 +0530

    Update useRestraunt.js

--- src/utils/useRestraunt.js
+++ src/utils/useRestraunt.js
@@ -10,17 +10,30 @@ fetchData();
 //     const req=await fetch(LISTOFRESTAURANT)
 // const json=await req.json();
 // setResList(json)
-        const req = fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01420&lng=76.99410&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')}`)
-  .then(response => {
-    if (response.ok) return response.json();
-    throw new Error('Network response was not ok.');
-  })
-  .then(data => {
-    const jsonObject = JSON.parse(data.contents);
-    console.log(jsonObject);
-    setResList(jsonObject)
-    // Now jsonObject is a JavaScript object
-  })
+     try {
+        const res = await fetch("https://handler-cors.vercel.app/fetch", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+        url: LISTOFRESTAURANT, //Replace 
+        })
+        });
+        
+        if (!res.ok) {
+        throw new Error(`Error: ${res.status} ${res.statusText}`);
+        }
+        
+        
+        const raw = await res.json();
+        setResList(raw)
+        console.log(raw); // see raw in console
+        
+        } 
+        catch (error) {
+        console.error("Error fetching data:", error);
+        }
   .catch(error => console.error('Error:', error));
 }
 return resList
