174cd669817b87f9e7acbd8a51a8325775d89684
cooldev07 <gogulinve@gmail.com>
2024-07-28 20:31:44 +0530

    Update useRestrauntMenu.js

--- src/utils/useRestrauntMenu.js
+++ src/utils/useRestrauntMenu.js
@@ -8,10 +8,20 @@ const useRestrauntMenu=function(resId){
 
 
     async function fetchData(){
-        const req=await fetch(MENU+resId);
-        const r=await req.json()
-        setRestaurant(r)
-        console.log(r)
+        // const req=await fetch(MENU+resId);
+        // const r=await req.json()
+        // setRestaurant(r)
+        // console.log(r)
+          fetch(`https://api.allorigins.win/get?url=${encodeURIComponent("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.01420&lng=76.99410&restaurantId="+resId)}`)
+        .then(response => {
+            if (response.ok) return response.json()
+            throw new Error('Network response was not ok.')
+        })
+        .then(data => {
+            const jsonObject = JSON.parse(data.contents);
+            console.log(jsonObject);
+            setRestaurant(jsonObject)
+        }).catch(error => console.error('Error:', error));
     }
 return restaurant;
 }
