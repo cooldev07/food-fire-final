9df945ee42a5cf2c165abc323b28e7ed97b1d68f
cooldev07 <gogulinve@gmail.com>
2024-07-28 18:08:34 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const LOGO="https://foodfire-app.netlify.app/foodFireLogo.adc19524.png";
-export const LISTOFRESTAURANT="https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01420&lng=76.99410&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-export const MENU="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.01420&lng=76.99410&restaurantId=";
-export const DISH_IMAGE="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+export const LOGO="https://corsproxy.io/?https://foodfire-app.netlify.app/foodFireLogo.adc19524.png";
+export const LISTOFRESTAURANT="https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01420&lng=76.99410&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const MENU="https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.01420&lng=76.99410&restaurantId=";
+export const DISH_IMAGE="https://corsproxy.io/?https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
